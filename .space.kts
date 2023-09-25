d64423102ffa4403e8eb8680e4331788faac0816
Julius König <julius@koenig-site.de>
2023-09-25 22:27:56 +0200

    work on automated deployment

--- .space.kts
+++ .space.kts
@@ -4,13 +4,33 @@
  * For more info, see https://www.jetbrains.com/help/space/automation.html
  */
 
+job("Prepare testcontainer image") {
+    // do not run on git push
+    startOn {
+        gitPush { enabled = false }
+    }
+
+    kaniko {
+        build {
+            file = "./tests/Dockerfile"
+            labels["vendor"] = "bastelquartier.de"
+        }
+
+        push("bastelquartier.registry.jetbrains.space/p/fapi-el/testcontainer/testcontainer") {
+            tags {
+                +"0.0.1"
+            }
+        }
+    }
+}
+
 job("Example shell script") {
     container(displayName = "Say Hello", image = "ubuntu") {
         shellScript {
             content = """
                 echo Hello
                 echo World!
-                python setup.py sdist
+                #python setup.py sdist
             """
         }
     }
