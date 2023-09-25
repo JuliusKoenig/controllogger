e105d1b27de3b0d0f6e76de3a382b4e3215c8dc3
Julius König <julius@koenig-site.de>
2023-09-26 00:57:11 +0200

    work on automated deployment

--- .space.kts
+++ .space.kts
@@ -50,7 +50,7 @@ job("Build and publish to Space") {
         shellScript {
             content = """
                 echo Build package...
-                python -m build
+                python setup.py -bV ${'$'}JB_SPACE_EXECUTION_NUMBER
                 echo Upload package...
                 twine upload --repository-url ${'$'}URL -u ${'$'}JB_SPACE_CLIENT_ID -p ${'$'}JB_SPACE_CLIENT_SECRET dist/*
             """
